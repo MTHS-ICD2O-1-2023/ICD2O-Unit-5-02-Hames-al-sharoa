02ef9bea596bdfbfc1d61ad8a0cc7d189f2fb531
Hames.Al-Sharoa <159185115+Hames-al-sharoa@users.noreply.github.com>
2024-06-04 18:38:24 +0000

    save all changes

--- js/script.js
+++ js/script.js
@@ -1,36 +1,33 @@
-// Copyright (c) 2024 hames.al-sharoa All rights reserved
+// Copyright (c) 2024 hames al sharoa All rights reserved
 //
-// Created by: hames.al-sharoa
-// Created on: Apr 2024
+// Created by: hames al sharoa 
+// Created on: may 224
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * Check service worker.
+ */
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICS2O-Unit5-02-JS/sw.js", {
+    scope: "/ICS2O-Unit5-02-JS/",
+  })
+}
 
 /**
  * This function displays the slider value.
  */
+function myButtonClicked() {
+  const buttonOnChecked = document.getElementById("flash1").checked
+  const randomNumber = Math.floor(Math.random() * 6) + 1
+  const randomNumber2 = Math.floor(Math.random() * -6) + 0
 
-function generateRandomNumber() {
-  // Get the value of the selected radio button
-  const radioButton = document.querySelector('input[name="numberType"]:checked');
-
-  // Check if a radio button is selected
-  if (!radioButton) {
-    alert('Please select a number type (Positive/Negative).');
-    return;
+  if (buttonOnChecked == true) {
+    document.getElementById("radio-button-value").innerHTML =
+      "Your positive number is: " + randomNumber
+  } else {
+    document.getElementById("radio-button-value").innerHTML =
+      "Your negative number is: " + randomNumber2
   }
-
-  // Get the value of the selected radio button (Positive/Negative)
-  const numberType = radioButton.value;
-  // Generate a random number
-  let randomNumber;
-  if (numberType === 'positive') {
-
-    // Generate a random positive number
-    randomNumber = Math.floor(Math.random() * 100) + 1; // Random number between 1 and 100
-  } else if (numberType === 'negative') {
-    // Generate a random negative number
-    randomNumber = -1 * (Math.floor(Math.random() * 100) + 1); // Random number between -1 and -100
-  }
-
-  // Display the generated random number
-  document.getElementById('output').innerHTML = `${randomNumber >= 0 ? '+' : ''}${randomNumber}`;
 }
